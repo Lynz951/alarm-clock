6fa16466d61767c73894b30810447e5d1a98ba39
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-09-16 02:09:08 +0000

    set alarm time

--- main.js
+++ main.js
@@ -1,31 +1,69 @@
+
+
 /* set variables */
 
-const display = document.getElementById('clock');
-/*
-const audio = new Audio(assets/01 Back to the Sea.m4a);
-audio.loop = true;
+const display = document.getElementById('current-time');
 let alarmTime = null;
-let alarmtimout = null;*/
+let alarmtimout = null;
 
 /* display current time */
 
 function updateTime() {
-    console.log('my update time function is running');
     const date = new Date();
 
     let hour = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
 
+
+    // Convert hours off military time
+
+function convertHour(time) {
+    if (hour > 12) {
+        hour = hour % 12;
+    }
+    return time;
+}
+ 
+
     display.innerText=`${hour} : ${minutes} : ${seconds}`;
 }
-updateTime();
 
-/* save alarm time 
+/* Receive a value and assign to alarm time*/
+
+function setAlarmTime(value) {
+    alarmTime = value;
+    console.log(alarmTime);
+}
+
+
+// function setAlarm() {
+//     console.log('my set alarm function is running');
+//     if (alarmTime) {
+//         const current = new Date();
+//         const timeToAlarm = new Date(alarmTime);
+
+//         if (timeToAlarm > current) {
+//             const timeout = timeToAlarm.getTime() - current.getTime();
+//             alarmTimeout = setTimeout(() => audio.play(), timout);
+//             alert('Alarm set');
+//         }
+//     }
+// }
+
+// function clearAlarm() {
+//     Audio.pause();
+//     if (alarmTimeout)
+// }
+
+
+updateTime();
+setInterval(updateTime, 1000);
+
 /* if there is an alarm time and time equals alarm time, sound the alarm, if not, do nothing */
 /* reset alarm when clear alarm button is pressed 
 /* run function every second */
 
-setInterval(updateTime, 1000);
+
 
 
