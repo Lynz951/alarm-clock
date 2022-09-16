fd217f9deccc279c3b6db76d11c0bc8f025f8a36
Lynz951 <lindsay.haddix@student.awesomeinc.org>
2022-09-16 17:46:07 +0000

    fixed military time

--- main.js
+++ main.js
@@ -1,55 +1,50 @@
-
-
 /* set variables */
 
-const display = document.getElementById('current-time');
+const display = document.getElementById("current-time");
 let alarmTime = null;
-let alarmtimout = null;
+let alarmTimeout = null;
 
 /* display current time */
 
 function updateTime() {
-    const date = new Date();
+  const date = new Date();
 
-    let hour = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
+  let hour = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
 
+  // Convert hours off military time- Doesn't work!
 
-    // Convert hours off military time
-
-function convertHour(time) {
+  // function convertHour(time) {
     if (hour > 12) {
-        hour = hour % 12;
+      hour = hour % 12;  /* NOTE REMEMBER! */
     }
-    return time;
-}
- 
-
-    display.innerText=`${hour} : ${minutes} : ${seconds}`;
+  
+  display.innerText = `${hour} : ${minutes} : ${seconds}`;
 }
 
 /* Receive a value and assign to alarm time*/
 
 function setAlarmTime(value) {
-    alarmTime = value;
-    console.log(alarmTime);
+  alarmTime = value;
+  console.log(alarmTime);
 }
 
 /* Set the dang alarm */
 
 function setAlarm() {
-    console.log('my set alarm function is running');
-    if (alarmTime) {
-        const current = new Date();
-        const timeToAlarm = new Date(alarmTime);
+  console.log("my set alarm function is running");
+  if (alarmTime) {
+    const current = new Date();
+    const timeToAlarm = new Date(alarmTime);
 
-        if (timeToAlarm > current) { /*Only sets alarm if time is after current time */
-            const timeout = timeToAlarm.getTime() - current.getTime();
-            alarmTimeout = setTimeout(() => timeout);
-            alert('Alarm set');
-        }
+    if (timeToAlarm > current) {
+      /*Only sets alarm if time is after current time */
+      const timeout = timeToAlarm.getTime() - current.getTime();
+      alarmTimeout = setTimeout(() => timeout);
+      alert("Alarm set");
     }
+  }
 }
 
 /* if there is an alarm time and time equals alarm time, sound the alarm, if not, do nothing */
@@ -59,25 +54,18 @@ function setAlarm() {
 //         date.getHours() === futureDate.getHours() &&
 //         date.getMinutes() === futureDate.getMinutes();
 //         alert('Wake up');
-//     ) 
+//     )
 // }
 
 /* reset alarm when clear alarm button is pressed */
 
 function clearAlarm() {
-    if (alarmTimeout) {
-        clearTimeout(alarmTimeout);
-        alert('Alarm cleared');
-}
+  if (alarmTimeout) {
+    clearTimeout(alarmTimeout);
+    alert("Alarm cleared");
+  }
 }
 
 /* run updateTime function every second */
 updateTime();
 setInterval(updateTime, 1000);
-
-
-
-
-
-
-
